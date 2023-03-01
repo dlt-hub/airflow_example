40cbdbf3b5854c871e255648d1be4811ff4aaaeb
Adrian <Adrian>
2023-03-01 17:57:47 +0100

    dbt for pipeline

--- dags/local_test_pipedrive.py
+++ dags/local_test_pipedrive.py
@@ -2,36 +2,24 @@ import dlt
 from pipedrive import pipedrive_source
 
 
-def load_pipedrive():
-    """Constructs a pipeline that will load all pipedrive data"""
-    # configure the pipeline with your destination details
-    pipeline = dlt.pipeline(pipeline_name='pipedrive2', destination='bigquery', dataset_name='pipedrive_data')
-    load_info = pipeline.run(pipedrive_source())
-    print(load_info)
-
-
-def load_selected_data():
-    """Shows how to load just selected tables using `with_resources`"""
-    pipeline = dlt.pipeline(pipeline_name='pipedrive', destination='postgres', dataset_name='pipedrive_data')
-    load_info = pipeline.run(pipedrive_source().with_resources("products", "deals", "dealFields", "deals_participants"))
-    print(load_info)
-    # just to show how to access resources within source
-    pipedrive_data = pipedrive_source()
-    # print source info
-    print(pipedrive_data)
-    print()
-    # list resource names
-    print(pipedrive_data.resources.keys())
-    print()
-    # print `persons` resource info
-    print(pipedrive_data.resources["deals_participants"])
-    print()
-    # alternatively
-    print(pipedrive_data.deals_participants)
-
-
 if __name__ == "__main__" :
     # run our main example
     # load_pipedrive()
     # load selected tables and display resource info
-    load_pipedrive()
+    pipeline = dlt.pipeline(pipeline_name='pipedrive2', destination='bigquery', dataset_name='pipedrive_data')
+
+    load_info = pipeline.run(pipedrive_source())
+    print(load_info)
+
+    pipeline = dlt.pipeline(pipeline_name='pipedrive2', destination='bigquery', dataset_name='pipedrive_dbt')
+    # now that data is loaded, let's transform it
+    # make or restore venv for dbt, uses latest dbt version
+    venv = dlt.dbt.get_venv(pipeline)
+    # get runner, optionally pass the venv
+    dbt = dlt.dbt.package(pipeline,
+        "/Users/adrian/PycharmProjects/dlthub/airflow_example/dags/pipedrive/dbt_pipedrive/pipedrive",
+        venv=venv)
+    models = dbt.run_all()
+    for m in models:
+        print(f"Model {m.model_name} materialized in {m.time} with status {m.status} and message {m.message}")
+
--- dags/pipedrive/dbt_pipedrive/pipedrive/models/model/persons.sql
+++ dags/pipedrive/dbt_pipedrive/pipedrive/models/model/persons.sql
@@ -2,6 +2,13 @@
 
 {{ config(materialized='table') }}
 
+-- get the first non null phone and email of the person in the table.
 
-select *
-from {{ source('raw', 'persons') }} as a
+SELECT p.*, pe.value as email, pp.value as phone
+FROM {{ source('raw', 'persons') }} as p
+left join {{ source('raw', 'persons__email') }} as pe
+on p._dlt_id=pe._dlt_parent_id and pe._dlt_list_idx = 0 and pe.value!=''
+left join {{ source('raw', 'persons__phone') }} as pp
+on p._dlt_id=pe._dlt_parent_id and pp._dlt_list_idx = 0 and pp.value!=''
+
+
