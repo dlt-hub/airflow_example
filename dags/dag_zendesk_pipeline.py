274326b7e3a98600b9fadfbc1bc005f3ee3b9015
Adrian <Adrian>
2023-03-31 17:35:35 +0200

    example dag

---
+++ dags/dag_zendesk_pipeline.py
@@ -0,0 +1,34 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from zendesk_pipeline import incremental_pipeline
+
+
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email': 'adrian@dlthub.com',
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 3,
+    'retry_delay': timedelta(minutes=1),
+    'start_date': datetime(2023, 2, 21)
+}
+
+dag = DAG(dag_id='Zendesk',
+          default_args=default_args,
+          schedule_interval='00 2 * * *',
+          max_active_runs=1,
+          catchup=False)
+
+
+load_task = PythonOperator(
+        task_id="load_Zendesk",
+        python_callable=incremental_pipeline,
+        trigger_rule="all_done",
+        retries=0,
+        dag=dag)
+
+load_task
+
