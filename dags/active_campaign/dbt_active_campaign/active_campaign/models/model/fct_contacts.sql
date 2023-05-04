44b19750869fc0da4466a89930c771c4c7bdbf09
v-grun <viktor.grunwald@gmail.com>
2023-05-04 11:29:11 +0200

    added dbt transformation for active campaign

---
+++ dags/active_campaign/dbt_active_campaign/active_campaign/models/model/fct_contacts.sql
@@ -0,0 +1,19 @@
+
+
+{{ config(materialized='table') }}
+
+-- get all contact details and their company names.
+SELECT
+  contacts.id,
+  contacts.first_name,
+  contacts.last_name,
+  accounts.name as company_name,
+  accounts.id as account_id,
+  email,
+  phone,
+  DATE(date_trunc(contacts.created_timestamp, MONTH)) as created_month
+FROM {{ source('active_campaign_data', 'account_contacts'}} AS account_contacts
+LEFT JOIN {{ source('active_campaign_data', 'contacts'}} AS contacts
+    ON contacts.id = account_contacts.contact
+LEFT JOIN {{ source('active_campaign_data', 'accounts'}} AS accounts
+    ON account_contacts.account = accounts.id
