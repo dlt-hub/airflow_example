be139480b62bf199493ca905a95903d7f29e57ab
v-grun <viktor.grunwald@gmail.com>
2023-05-04 16:04:59 +0200

    changed sql query

--- dags/active_campaign/dbt_active_campaign/active_campaign/models/model/fct_contacts.sql
+++ dags/active_campaign/dbt_active_campaign/active_campaign/models/model/fct_contacts.sql
@@ -12,8 +12,16 @@ SELECT
   email,
   phone,
   DATE(date_trunc(contacts.created_timestamp, MONTH)) as created_month
-FROM {{ source('active_campaign_dbt_raw', 'account_contacts')}} AS account_contacts
-LEFT JOIN {{ source('active_campaign_dbt_raw', 'contacts')}} AS contacts
-    ON contacts.id = account_contacts.contact
-LEFT JOIN {{ source('active_campaign_dbt_raw', 'accounts')}} AS accounts
-    ON account_contacts.account = accounts.id
+FROM
+  `pure-ace-372509.active_campaign_dbt_raw.account_contacts` AS account_contacts
+LEFT JOIN
+  `pure-ace-372509.active_campaign_dbt_raw.contacts` AS contacts
+ON
+  contacts.id = account_contacts.contact
+LEFT JOIN
+  `pure-ace-372509.active_campaign_dbt_raw.accounts` AS accounts
+ON
+  account_contacts.account = accounts.id
+  --ORDER BY `pure-ace-372509.active_campaign_dbt_raw.contacts`.id ASC
+LIMIT
+  1000
